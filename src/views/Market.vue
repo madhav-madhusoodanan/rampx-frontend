09484dab3ae34d6151fe01a0442a006e1fbfabc8
Madhav Madhusoodanan <f20200049@pilani.bits-pilani.ac.in>
2024-10-25 00:09:47 +0530

    chore: updating route components for better visibility

--- src/views/Market.vue
+++ src/views/Market.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="about bg-green-200 grid place-items-center h-[91vh]">
-    <h1 class="text-2xl">This is trade page</h1>
+    <h1 class="text-2xl">This is market page</h1>
   </div>
 </template>
